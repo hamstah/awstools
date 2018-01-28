36146449b0a41d7f84de58448f31295bbb5e3cd4
hamstah <hamstahguru@gmail.com>
2018-01-28 22:31:07 +0000

    Strip go builds

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,6 +10,6 @@ find ${base} -name "main.go" | while read src; do
     src=$(realpath --relative-to=${base} ${src})
     name=bin/$(echo ${src} | awk -F/ '{print $1"-"$2}')
     echo "  ${name}"
-    CGO_ENABLED=0 go build -installsuffix cgo -o ${base}/${name} ${src}
+    CGO_ENABLED=0 go build -installsuffix cgo -o ${base}/${name} -ldflags="-s -w" ${src}
     gpg --armor --detach-sig ${base}/${name}
 done
