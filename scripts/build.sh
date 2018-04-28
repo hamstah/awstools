068540d94cc395dbb067c77269b2b331dd357634
Nico Esteves <git@hamstah.com>
2018-04-29 00:06:05 +0200

    Updated build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,12 +4,20 @@ set -e
 base=$(dirname $0)/..
 
 mkdir -p ${base}/bin
+rm ${base}/bin/*.asc
 
 echo "Building"
 find ${base} -name "main.go" | while read src; do
     src=$(realpath --relative-to=${base} ${src})
     name=bin/$(echo ${src} | awk -F/ '{print $1"-"$2}')
     echo "  ${name}"
-    CGO_ENABLED=0 go build -installsuffix cgo -o ${base}/${name} -ldflags="-s -w" ${src}
-    gpg --armor --detach-sig ${base}/${name}
+    folder=`dirname ${src}`
+    if [ ! -f ${folder}/Makefile ]; then
+	CGO_ENABLED=0 go build -installsuffix cgo -o ${base}/${name} -ldflags="-s -w" ${folder}/*.go
+	gpg --armor --detach-sig ${base}/${name}
+    else
+	cd ${folder}
+	make
+	cd -
+    fi
 done
