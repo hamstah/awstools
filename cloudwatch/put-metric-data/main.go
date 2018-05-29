405b39f90e1ea8834e7f242e8a727817c766e0aa
Nico Esteves <git@hamstah.com>
2018-05-29 06:41:46 +0200

    Added dimensions to cloudwatch-put-metric-data

--- cloudwatch/put-metric-data/main.go
+++ cloudwatch/put-metric-data/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/cloudwatch"
 	"github.com/hamstah/awstools/common"
 	kingpin "gopkg.in/alecthomas/kingpin.v2"
@@ -10,6 +11,7 @@ var (
 	flags      = common.KingpinSessionFlags()
 	metricName = kingpin.Flag("metric-name", "Name of the Cloudwatch metric").Required().String()
 	namespace  = kingpin.Flag("namespace", "Name of the Cloudwatch namespace").Required().String()
+	dimensions = kingpin.Flag("dimension", "Dimensions name=value").StringMap()
 	value      = kingpin.Flag("value", "Metric value").Required().Float64()
 )
 
@@ -20,10 +22,19 @@ func main() {
 
 	session, conf := common.OpenSession(flags)
 
+	cloudwatchDimensions := []*cloudwatch.Dimension{}
+	for name, value := range *dimensions {
+		cloudwatchDimensions = append(cloudwatchDimensions, &cloudwatch.Dimension{
+			Name:  aws.String(name),
+			Value: aws.String(value),
+		})
+	}
+
 	cloudwatchClient := cloudwatch.New(session, conf)
 	_, err := cloudwatchClient.PutMetricData(&cloudwatch.PutMetricDataInput{
 		MetricData: []*cloudwatch.MetricDatum{
 			{
+				Dimensions: cloudwatchDimensions,
 				MetricName: metricName,
 				Value:      value,
 			},
