ce46c30df6921b9efbe5e88430fe016e50574c69
Nico Esteves <git@hamstah.com>
2019-06-21 00:10:39 +0200

    kms-env: Refactor

--- kms/env/main.go
+++ kms/env/main.go
@@ -27,7 +27,122 @@ var (
 	ssmPrefix                  = kingpin.Flag("ssm-prefix", "Prefix for the SSM environment variables").Default("SSM_").String()
 	secretsManagerPrefix       = kingpin.Flag("secrets-manager-prefix", "Prefix for the secrets manager environment variables").Default("SECRETS_MANAGER_").String()
 	secretsManagerVersionStage = kingpin.Flag("secrets-manager-version-stage", "The version stage of secrets from secrets manager").Default("AWSCURRENT").String()
+	refreshInterval            = kingpin.Flag("refresh-interval", "Refresh interval").Default("0").Duration()
 )
+
+type Source struct {
+	Name       string
+	Identifier string
+}
+
+type Config struct {
+	Sources           map[string][]Source
+	StaticEnvironment map[string]string
+}
+
+func (c *Config) Refresh() (map[string]string, error) {
+	env := map[string]string{}
+
+	session, conf := common.OpenSession(flags)
+
+	for secretType, sources := range c.Sources {
+		switch secretType {
+		case "SSM":
+			ssmClient := ssm.New(session, conf)
+			for _, source := range sources {
+				if strings.HasSuffix(source.Identifier, "/*") {
+					values, err := getParametersByPath(ssmClient, source.Identifier[:len(source.Identifier)-2], source.Name)
+					if err != nil {
+						return nil, err
+					}
+					for key, value := range values {
+						env[key] = value
+					}
+				} else {
+					value, err := ssmFetch(ssmClient, source.Identifier)
+					if err != nil {
+						return nil, err
+					}
+					env[source.Name] = value
+				}
+			}
+		case "SECRETS_MANAGER":
+			secretsManagerClient := secretsmanager.New(session, conf)
+			for _, source := range sources {
+				values, err := fetchSecret(secretsManagerClient, source.Identifier, source.Name)
+				if err != nil {
+					return nil, err
+				}
+				for key, value := range values {
+					env[key] = value
+				}
+			}
+		case "KMS":
+			kmsClient := kms.New(session, conf)
+			for _, source := range sources {
+				value, err := kmsDecrypt(kmsClient, source.Identifier)
+				if err != nil {
+					return nil, err
+				}
+				env[source.Name] = value
+			}
+		}
+	}
+
+	for key, value := range c.StaticEnvironment {
+		env[key] = value
+	}
+
+	return env, nil
+}
+
+func ParseConfig(env []string) (*Config, error) {
+	config := &Config{
+		Sources:           map[string][]Source{},
+		StaticEnvironment: map[string]string{},
+	}
+
+	prefixes := map[string]string{
+		"KMS":             *kmsPrefix,
+		"SSM":             *ssmPrefix,
+		"SECRETS_MANAGER": *secretsManagerPrefix,
+	}
+	for _, value := range env {
+		parts := strings.SplitN(value, "=", 2)
+		if len(parts) != 2 {
+			continue
+		}
+
+		key := parts[0]
+		value := parts[1]
+
+		found := false
+		for secretType, prefix := range prefixes {
+			if strings.HasPrefix(key, prefix) {
+				key := key[len(prefix):]
+
+				name := key
+				if strings.HasPrefix(key, "_") {
+					name = ""
+				}
+
+				config.Sources[secretType] = append(config.Sources[secretType], Source{
+					Name:       name,
+					Identifier: value,
+				})
+				found = false
+				break
+			}
+		}
+
+		if !found {
+			config.StaticEnvironment[key] = value
+		}
+
+	}
+
+	return config, nil
+}
 
 func main() {
 	kingpin.CommandLine.Name = "kms-env"
@@ -35,24 +150,16 @@ func main() {
 	kingpin.Parse()
 	common.HandleInfoFlags(infoFlags)
 
-	session, conf := common.OpenSession(flags)
-	kmsClient := kms.New(session, conf)
-	ssmClient := ssm.New(session, conf)
-	secretsManagerClient := secretsmanager.New(session, conf)
-
 	env := os.Environ()
-	var pEnv []string
-	for _, value := range env {
-		parts := strings.SplitN(value, "=", 2)
-		if len(parts) != 2 {
-			continue
-		}
 
-		result, err := handleEnvVar(kmsClient, ssmClient, secretsManagerClient, parts[0], parts[1])
-		common.FatalOnError(err)
-		for newKey, newValue := range result {
-			pEnv = append(pEnv, fmt.Sprintf("%s=%s", newKey, newValue))
-		}
+	config, err := ParseConfig(env)
+	common.FatalOnError(err)
+
+	envMap, err := config.Refresh()
+	common.FatalOnError(err)
+	var pEnv []string
+	for key, value := range envMap {
+		pEnv = append(pEnv, fmt.Sprintf("%s=%s", key, value))
 	}
 
 	p := exec.Command((*command)[0], (*command)[1:]...)
@@ -60,44 +167,9 @@ func main() {
 	p.Stdin = os.Stdin
 	p.Stderr = os.Stderr
 	p.Stdout = os.Stdout
-	err := p.Run()
+	p.Run()
 
-	// TODO: When https://github.com/golang/go/commit/be94dac4e945a2921b116761e41f1c22f0af2add is released, replace the below with
-	// os.Exit(p.ProcessState.ExitCode())
-	os.Exit(common.GetExitCode(p, err))
-}
-
-func handleEnvVar(kmsClient *kms.KMS, ssmClient *ssm.SSM, secretsManagerClient *secretsmanager.SecretsManager, key, value string) (map[string]string, error) {
-	if strings.HasPrefix(key, *kmsPrefix) {
-		newValue, err := kmsDecrypt(kmsClient, value)
-		if err != nil {
-			return nil, err
-		}
-		return map[string]string{key[len(*kmsPrefix):]: newValue}, nil
-	} else if strings.HasPrefix(key, *ssmPrefix) {
-		if strings.HasSuffix(value, "/*") {
-			prefix := key[len(*ssmPrefix):]
-			// remove prefix if starts with _
-			if strings.HasPrefix(prefix, "_") {
-				prefix = ""
-			}
-			return getParametersByPath(ssmClient, value[:len(value)-2], prefix)
-		} else {
-			newValue, err := ssmFetch(ssmClient, value)
-			if err != nil {
-				return nil, err
-			}
-			return map[string]string{key[len(*ssmPrefix):]: newValue}, nil
-		}
-	} else if strings.HasPrefix(key, *secretsManagerPrefix) {
-		prefix := key[len(*secretsManagerPrefix):]
-		if strings.HasPrefix(prefix, "_") {
-			prefix = ""
-		}
-		return fetchSecret(secretsManagerClient, value, prefix)
-	}
-
-	return map[string]string{key: value}, nil
+	os.Exit(p.ProcessState.ExitCode())
 }
 
 func getParametersByPath(client *ssm.SSM, path string, prefix string) (map[string]string, error) {
