43ec58f0307c2d8a52da57857651c6619ddfb661
Nico Esteves <git@hamstah.com>
2019-07-20 07:49:10 +0200

    common: Added FatalOnErrorW

--- common/utils.go
+++ common/utils.go
@@ -4,12 +4,19 @@ import (
 	"encoding/json"
 	"io/ioutil"
 
+	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
 )
 
 func FatalOnError(err error) {
 	if err != nil {
 		log.Fatalln(err)
+	}
+}
+
+func FatalOnErrorW(err error, msg string) {
+	if err != nil {
+		log.Fatalln(errors.Wrap(err, msg))
 	}
 }
 
