2ac56f70ffa52b551f458cf363ab593f927f838e
Nico Esteves <git@hamstah.com>
2019-07-26 14:26:10 +0200

    aws-dump: Added ec2:key-pairs report

--- aws/dump/ec2.go
+++ aws/dump/ec2.go
@@ -18,6 +18,7 @@ var (
 			"images":          EC2ListImages,
 			"instances":       EC2ListInstances,
 			"nat-gateways":    EC2ListNATGateways,
+			"key-pairs":       EC2ListKeyPairs,
 		},
 	}
 )
@@ -207,4 +208,28 @@ func EC2ListNATGateways(session *Session) *ReportResult {
 		})
 
 	return &ReportResult{resources, err}
+}
+
+func EC2ListKeyPairs(session *Session) *ReportResult {
+	client := ec2.New(session.Session, session.Config)
+
+	keypairs := []Resource{}
+
+	res, err := client.DescribeKeyPairs(&ec2.DescribeKeyPairsInput{})
+	if err != nil {
+		return &ReportResult{nil, err}
+	}
+
+	for _, keypair := range res.KeyPairs {
+		keypairs = append(keypairs, Resource{
+			ID:        *keypair.KeyName,
+			Service:   "ec2",
+			Type:      "key-pair",
+			AccountID: session.AccountID,
+			Region:    *session.Config.Region,
+			Metadata:  structs.Map(keypair),
+		})
+	}
+
+	return &ReportResult{keypairs, err}
 }
